879f403c39a911eba6f044c33040bac69db24153
alodahl <alodahl@pointloma.edu>
2017-11-08 22:43:38 -0800

    dashboard timers display seconds, minutes, and hours; timers start and stop when clicked

--- public/index.js
+++ public/index.js
@@ -1,11 +1,73 @@
 "use strict";
 
+//formats seconds into HOUR:MIN:SEC
+const formatSeconds = (seconds) => {
+  var date = new Date(null);
+  date.setSeconds(seconds); // specify value for SECONDS here
+  return date.toISOString().substr(11, 8);
+}
 
+//makes one new blank timer with an id of it's index in the timers array
+const TimerComponent = (timer, index) =>
+  `<div class="timer" data-index="${index}">
+    <div class="timer-info">
+      <h3 class="timer-label">NEW PROJECT</h3>
+      <p class="timer-stats">Today's Total:${timer.todaysTime}s</p>
+      <p class="timer-stats">Project Total: ${timer.totalTime}</p>
+    </div>
+    <div class="timer-button button" data-id="${index}">${formatSeconds(timer.todaysTime)}</div>
+  </div>`;
+
+//initialize timers array with one timer object
+let timers = [{
+  totalTime: 0,
+  todaysTime: 0,
+  isRunning: false
+}];
+const newTimer = {
+  totalTime: 0,
+  todaysTime: 0,
+  isRunning: false
+};
+let intervalTicker;
+
+//listener for square timer button. on click, if it was off, it turns on timer and adds
+//1 to the totalTime and todaysTime every second. If it was on, it should stop the timer.
 $('.timer-section').on('click', '.timer-button', function(event) {
-var start = new Date;
+  var start = new Date;
+  let id = $(this).attr('data-id');
+  let clickedTimer = timers[id];
 
-setInterval(function(event) {
-    $('.timer-button').text(Math.round((new Date - start) / 1000)+ "s");
-}, 1000);
-
+  if (!timers[id].isRunning) {
+    clickedTimer.isRunning = true;
+    intervalTicker = setInterval(function(event) {
+        clickedTimer.todaysTime += 1;
+        renderTimers(timers);
+    }, 1000);
+  } else {
+    clickedTimer.isRunning = false;
+    clearInterval(intervalTicker);
+  }
+  console.log(`timer ${id}`)
 })
+
+//when "new timer" area is clicked, add a new object to timers array
+//and re-render the whole array of timers.
+$('.new-timer-button').on('click', function() {
+  timers.push(newTimer);
+  renderTimers(timers);
+})
+
+//render each object in timers array by passing values into a
+//timerComponent html block and then appending that to the dom.
+function renderTimers(timers) {
+  $('.timer-section').html('');
+  for(let i=0; i < timers.length; i++) {
+    let index = i;
+    let timer = timers[i];
+    $('.timer-section').append(TimerComponent(timer, index))
+  }
+}
+
+//render existing timers on page load
+renderTimers(timers);
--- to-do.md
+++ to-do.md
@@ -1,7 +1,11 @@
 # Project Timer To-do
 
 ## Incomplete Tasks:
-- Make landing page responsive for large widths
+- disconnect timers 2 thru 4
+- style running timers to be green
 
 
 ## Completed Tasks:
+- Make landing page responsive for large widths
+- use jquery to have timers display seconds, minutes, and hours.
+- base timer rendering on index
